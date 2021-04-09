01adf4b02d9534fc12b9caac1adaab27eb425f64
Ed Pike <edpike365@gmail.com>
2021-04-09 12:36:43 -0700

    more tries at env vars on netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   // We do not need to require dotenv because it was already done in gatsby-config.js ?
   const blogStatusList = `${process.env.BLOG_STATUS}`;
+  console.info(" gatsby-node.js blogStatusList = " + blogStatusList )
 
   // Get all markdown blog posts sorted by date
   const result = await graphql(
