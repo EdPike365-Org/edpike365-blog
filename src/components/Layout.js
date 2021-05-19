9588684303bcafaa77539fffaf2f96fe0ed91ac0
Ed Pike <edpike365@gmail.com>
2021-05-18 17:39:24 -0700

    Had not added files with name changes to the commit. DOH.

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Global } from "@emotion/react"
+import { coreTheme } from "../styles/coreTheme.js"
+import LayoutGrid from "./LayoutGrid"
+import Header from "./Header"
+import NavBar from "./NavBar"
+import SideBar from "./SideBar"
+import Main from "./Main"
+import Footer from "./Footer"
+import { NavContextProvider} from "../contexts/NavContext"
+
+const Layout = ( {children, location }) => {
+
+  // If we are on the home page, make the logos not be links
+  // I'm only doing this as a demonstration
+  // location object must be passed from pages into components
+  /*
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+*/
+  return (
+    <NavContextProvider >
+      <Global styles={coreTheme} />
+      <LayoutGrid >
+        <Header />
+        <NavBar />
+        <Main>{children}</Main>
+        <SideBar />
+        <Footer />
+      </LayoutGrid>
+    </NavContextProvider>
+  )
+}
+
+export default Layout
