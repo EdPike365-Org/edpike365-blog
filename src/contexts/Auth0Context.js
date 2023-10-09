612458bac4b9783cf81750cbf8467566f2d0e422
Ed Pike <edpike365@gmail.com>
2023-10-08 17:46:19 -0700

    Fixed Auth0 logout redirect being ignored.

--- src/contexts/Auth0Context.js
+++ src/contexts/Auth0Context.js
@@ -1,32 +1,30 @@
 import React from "react"
-import { Auth0Provider } from '@auth0/auth0-react';
 import { navigate } from 'gatsby';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 // This lets us redirect the user to the page they were trying to get to
 // before they were asked to log in.
+// IFFF they were not trying to get to a specific page,
+// the appState will be null, and we will redirect them to the home page.
 // https://auth0.com/docs/libraries/auth0-react#redirecting-after-login
 const onRedirectCallback = (appState) => {
     // Use Gatsby's navigate method to replace the url
-    console.log("Auth0Context onRedirectCallback appState = " + appState)
-    navigate(appState?.returnTo || '/', { replace: true });
-};
+    navigate(appState?.returnTo || '/', { replace: true })
+}
 
-// we have multiple states in one context
 export const Auth0Context = element => {
-    
-    // window.location.origin is the protocol + hostname + port
-    //console.log("Auth0Context window.location.origin = " + window.location.origin)
-    
+
     const redirectURI = window.location.origin + "/callbacks/auth"
-    //console.log("Auth0Context redirectURI = " + redirectURI)
-     
+
     return (
         <Auth0Provider
             domain={process.env.GATSBY_AUTH0_DOMAIN}
             clientId={process.env.GATSBY_AUTH0_CLIENTID}
-            authorizationParams={{ 
+            authorizationParams={{
                 redirect_uri: redirectURI
                 // we can add other arbitrary key: values here
+                // but I have not figured out how to pull them out
+                // on the callback page
             }}
             onRedirectCallback={onRedirectCallback}
         >
