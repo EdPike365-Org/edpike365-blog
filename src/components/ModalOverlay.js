3f0d31a93735086ed1282cf13e82062102797db8
Ed Pike <edpike365@gmail.com>
2023-10-19 17:14:32 -0700

    Fixed build error on overlay.

--- src/components/ModalOverlay.js
+++ src/components/ModalOverlay.js
@@ -3,9 +3,13 @@ import { css } from "@emotion/react"
 
 //onKeyDown={this.handleClick} is to prevent a11y: warning  Visible, non-interactive elements with click handlers must have at least one keyboard listener  jsx-a11y/click-events-have-key-events
 //role="presentation" is to prevent a11y: warning  Avoid non-native interactive elements. If using native HTML is not possible, add an appropriate role and support for tabbing, mouse, keyboard, and touch inputs to an interactive content element  jsx-a11y/no-static-element-interactions
+const handleClick = (event) => {
+    event.stopPropagation();
+}
+
 const ModalOverlay = ({children}) => {
     return (
-        <div css={modalDivCSS} onClick={event => event.stopPropagation()} onKeyDown={this.handleClick} role="presentation">
+        <div css={modalDivCSS} onClick={handleClick} onKeyDown={handleClick} role="presentation">
             {children}
         </div>
     )
